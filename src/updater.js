db0d7d786dbb73c4102b7f410c7097cfccf3fd9f
Kodi Craft <kodicraft4@gmail.com>
2022-03-01 16:50:37 +0100

    Fixed update checking

--- src/updater.js
+++ src/updater.js
@@ -1,4 +1,4 @@
-const MYVERSION = 323
+const MYVERSION = 325
 
 console.log("Checking for updates...")
 axios.get("https://api.github.com/repos/KodiCraft/hentai.js/releases")
@@ -6,10 +6,11 @@ axios.get("https://api.github.com/repos/KodiCraft/hentai.js/releases")
     console.log(res)
     body = res.data[0].body
     try {
-      id = body.match(/\[\[\]\]/)[0]
+      id = body.match(/(?<=\[\[).*?(?=]])/)[0]
     } catch {
       id = 0 // Could not get update data
     }
+    console.log("Most recent version is " + id)
     if (id > MYVERSION) {
       if (confirm("A new version of hentai.js is available!\nWould you like to open the github releases page?")) {
         require("electron").shell.openExternal("https://github.com/KodiCraft/hentai.js/releases")
